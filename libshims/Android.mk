e6016befc48262ed9a4c44dbd4aa7753dce041e2
billchen1977@gmail.com <billchen1977@gmail.com>
2017-09-02 00:21:40 +0800

    IMS: Update libshim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -23,3 +23,17 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := MediaBuffer.cpp
+
+LOCAL_SHARED_LIBRARIES          += libui libgui libstagefright_foundation
+LOCAL_C_INCLUDES                += framework/native/include frameworks/av/include
+LOCAL_CFLAGS                    += -Wno-unused-private-field
+
+LOCAL_MODULE := libshims_ims
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
