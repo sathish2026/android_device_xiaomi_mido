5169233ca65405d5c7061b33d0c35e854c5799dc
sathish2026 <txt2sathish@gmail.com>
2017-09-17 01:38:11 +0530

    config.fs

--- device.mk
+++ device.mk
@@ -156,10 +156,6 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
-# Fs_config
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # GNSS HAL
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
