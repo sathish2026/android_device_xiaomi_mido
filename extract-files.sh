82317a0686ffb36b9a9c3d1bc066b0c485580e29
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-12-17 22:50:15 +0800

    mido: update proprietary files list fom tissot
    * update all blobs but graphics
    * update paths for wcnss daemon and config
    * update audio_effects config

--- extract-files.sh
+++ extract-files.sh
@@ -58,7 +58,7 @@ fi
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
-extract "$MY_DIR"/proprietary-my.txt "$SRC" "$SECTION"
-#extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
 
 "$MY_DIR"/setup-makefiles.sh
