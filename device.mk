f5e50654d8653c88f7e36c17c8d9d99e878b6d96
sathish2026 <txt2sathish@gmail.com>
2018-03-03 11:28:33 +0530

    mido: build SnapdragonCamera

--- device.mk
+++ device.mk
@@ -120,7 +120,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.msm8953 \
     libmm-qcamera \
-    Snap \
+    SnapdragonCamera \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
